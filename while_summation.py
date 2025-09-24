648f88d1a1c04c84fdb068d55814fc10b4a3b343
Dr-Harvester <Dr_Harvester@outlook.com>
2025-09-24 10:12:54 -0600

    Update while_summation.py

--- while_summation.py
+++ while_summation.py
@@ -1,1 +1,9 @@
 #while summation code following directions from Readme
+
+n = int(input("What is the last number?: "))
+i = 1
+sum = 0
+while i <= n:
+  sum += i
+  i += 1
+print("The sum is:", sum)
